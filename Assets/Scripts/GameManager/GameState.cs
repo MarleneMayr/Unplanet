eb226a613ec43f89cb44430a0afd1c1d58d22973
Marlene Mayr <marlene-mayr@live.com>
2020-12-19 12:17:04 +0100

    remove rendertexture postprocessing

--- Assets/Scripts/GameManager/GameState.cs
+++ Assets/Scripts/GameManager/GameState.cs
@@ -14,9 +14,8 @@ public class GameState : State
     public override void AfterActivate()
     {
         Spawn(spawnPoint);
-        UIcam.GetComponent<AudioListener>().enabled = false;
         player.gameObject.SetActive(true);
-        //UIcam.gameObject.SetActive(false);
+        UIcam.gameObject.SetActive(false);
 
         goal.OnReached.AddListener(EndGame);
     }
@@ -24,8 +23,7 @@ public class GameState : State
     public override void BeforeDeactivate()
     {
         player.gameObject.SetActive(false);
-        //UIcam.gameObject.SetActive(true);
-        UIcam.GetComponent<AudioListener>().enabled = true;
+        UIcam.gameObject.SetActive(true);
 
         menu.Hide();
         pauseMenu.Hide();
