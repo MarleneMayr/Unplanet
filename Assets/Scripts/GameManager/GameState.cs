42fb6456fbc46977ee38591f892a26140a5e11df
Marlene Mayr <marlene-mayr@live.com>
2020-12-21 14:23:24 +0100

    removed unused parts of goal vfx

--- Assets/Scripts/GameManager/GameState.cs
+++ Assets/Scripts/GameManager/GameState.cs
@@ -90,9 +90,9 @@ public class GameState : State
 
     private IEnumerator Euphoria(float seconds, float fadeIn)
     {
-        yield return new WaitForSeconds(seconds - 1);
+        yield return new WaitForSeconds(seconds - 0.5f);
         goal.StopEffects();
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(0.5f);
 
         // spawn goal at new position
         currentGoalPos = goal.Spawn(lvl.GetNextGoal());
